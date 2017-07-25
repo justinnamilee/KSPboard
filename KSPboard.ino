5eff08165e4e8ec3857c0d14aeb75f8e387db17a
justinnamilee <justin.nami.lee@gmail.com>
2017-07-25 11:11:18 -0300

    Updated Arduino driver.
    Removed magic numbers and did some thinking as to the type of control I want to have for pitch, yaw, roll, etc.

--- KSPboard.ino
+++ KSPboard.ino
@@ -1,43 +1,56 @@
 /// Justin Lee <justin at kitten dot pink>
 //
 // Arduino driver for the KSPboard game
-// controller.  See LICENSE and README.
+// controller.  See LICENSE and README
+// for more information.
 
-byte pinLED = 6, pinSwitch = 2;
-int delayTime = 10;
 
-byte pitch, yaw, roll, throttle;
+#define ENABLE 1
+#define DISABLE 0
+
+#define SERIAL_SPEED 115200
+
+#define PIN_LED 13
+
+#define LOOP_DELAY 1
+#define START_DELAY 5
+
+
+byte pinSwitch = 2;
+
+volatile byte pitch, yaw, roll, throttle;
 
 byte state = 1;
 
 
 void setupSerial()
 {
-  Serial.begin(115200);
+  Serial.begin(SERIAL_SPEED);
   Serial.println();
   Serial.flush();
 
+  Serial.print(1); // enable code, super secure
   Serial.print(1);
   Serial.print(1);
-  Serial.print(1);
-
-  delay(5);
 }
 
 
 void updateState()
 {
+  // tells the python client when to close
   state = digitalRead(pinSwitch);
-  digitalWrite(pinLED, state);
+  digitalWrite(PIN_LED, state);
 
   if (state)
-    Serial.print(1);
+    Serial.print(ENABLE);
   else
-    Serial.print(0);
+    Serial.print(DISABLE);
 }
 
 void updateHelm()
 {
+  // helm are always sent without enable code
+
   Serial.print(pitch);
   Serial.print(yaw);
   Serial.print(roll);
@@ -46,23 +59,30 @@ void updateHelm()
 
 void updateOps()
 {
-  Serial.print(0);
+  Serial.print(DISABLE);
 }
 
 
 void setup()
 {
-  pinMode(pinLED, OUTPUT); // debug led
+  pinMode(PIN_LED, OUTPUT); // debug led
+  digitalWrite(PIN_LED, HIGH);
+
   pinMode(pinSwitch, INPUT_PULLUP); // active-low switch
 
   setupSerial();
+
+  delay(START_DELAY);
 }
 
 void loop()
 {
- updateState();
- updateHelm();
- updateOps();
+  if (state)
+  {
+    updateState();
+    updateHelm();
+    updateOps();
+  }
 
- delay(1);
+  delay(LOOP_DELAY);
 }
